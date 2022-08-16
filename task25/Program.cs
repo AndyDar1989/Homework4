98d97468402c6c74fe642a28820ddadd6c55b117
AndrewDar <geologalone@gmail.com>
2022-08-16 20:54:49 +0300

    task folders created

---
+++ task25/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+
+int PowerNumber(int num1, int num2)
+{
+    int num1_pow = num1;
+    for (int count = 2; count <= num2; count++)
+        num1_pow = num1_pow * num1;
+    return num1_pow;
+}
+
+
+Console.Write("Enter number A: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number B: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int result = PowerNumber(number1, number2);
+Console.WriteLine($"the number {number1} to the power of {number2} is equal to {result} ");
+
---
+++ task27/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+
+int SumOfDigits(int num)
+{
+    return sum;
+}
+
+Console.Write("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
